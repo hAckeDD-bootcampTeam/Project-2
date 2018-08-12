3cbd1f8780756daed940747ae4f54101e648a9b0
David L <lapadula.david@gmail.com>
2018-08-12 11:54:32 -0400

    UI and selectors done

--- public/js/home.js
+++ public/js/home.js
@@ -1,135 +1,180 @@
 /* eslint-disable */
-
-
 $(document).ready(function () {
 
-    //List Filter
+    //instanitate list filter for searching through projects
     let options = {
         valueNames: ['projTags']
     };
-
     let userList = new List('project-tags', options);
 
-    // Hide both main div's on page load    
-    $('#cache-div').hide();
-    // $('#projs-div').hide(); 
-    $('#group-modal-settings').hide();
+    // Selectors for the interactive elements on the page 
+    let cacheBtn = $('#cache-btn');
+    let projBtn = $('#projs-btn');
 
-    // Toggle feature for both the main div's on the home page
-    $('#cache-btn').click(function () {
-        $('#cache-div').animate({
+    // User divs and project dashboard
+    let cacheDiv = $('#cache-div');
+    let projDiv = $('#projs-div');
+    let groupSettingsDiv = $('#group-modal-settings');
+    let groupInfoSection = $('#group-info-section');
+    let groupDashboardBtn = $('#group-settings-btn');
+
+    //Hide the divs when the page loads
+    cacheDiv.hide();
+    projDiv.hide();
+    groupSettingsDiv.hide();
+
+    // Toggle feature for the personal cache div
+    cacheBtn.click(function () {
+        cacheDiv.animate({
             width: 'toggle'
         });
-        $('#projs-div').hide();
+        projDiv.hide();
     });
 
-    $('#projs-btn').click(function () {
-        $('#projs-div').animate({
+    // Toggle feature for the project div
+    projBtn.click(function () {
+        projDiv.animate({
             width: 'toggle'
         });
-        $('#cache-div').hide();
+        cacheDiv.hide();
     });
 
-    // Toggle button for the project administration
-    $('#group-settings-btn').click(function () {
-        $('#group-modal-settings').animate({
+    // Dashboard admin features toggle
+    groupDashboardBtn.click(function () {
+        groupSettingsDiv.animate({
             height: 'toggle'
         });
-        $('#group-info-section').animate({
+        groupInfoSection.animate({
             height: 'toggle'
         });
     });
 
-    //Selectors
+    //User action buttons and selectors
     let logOutBtn = $('#logOut-btn');
     let userName = $('#user-name');
+
+    // Selectors for searching through reddit
     let searchredditBtn = $('#search-reddit-btn');
     let redditQuery = $('#search-reddit-input');
     let redditClear = $('#reddit-clear-btn');
 
+    // selectors to  populate the div with reddit queries
+    let redditQueryRow = $('.redditQueryRow'); //add a new row with limited amount of queries
+    let redditQueryCol = $('.reddit-query-col'); //append all queries to the row once the query has been made
+    // each selectorsed to populate the new row
+    let redditQueryTitle = $('.reddit-query-title')
+    let redditQueryURL = $('.reddit-query-url')
+    let redditQueryText = $('.reddit-query-text')
+
+    // Selectors for adding a new snippet to personal cache
     let newSnipName = $('#new-snip-name');
     let newSnipDesc = $('#new-snip-desc');
     let newSnipTag = $('#new-snip-tag');
     let addSnipBtn = $('#add-snip-btn');
 
-    let newProjName = $('#new-proj-name');
-    let newProjDesc = $('#new-proj-desc');
-    let newProjBtn = $('#new-proj-btn');
-    let projPublicCheck = $('#public-proj')
+    //SELECTORS FOR PERSONAL CACHE BEGIN
 
+    // Selector for user searching through a snippet in personal cache, filtered by either tag or text param - or both
     let searchTagInput = $('#search-tag-input');
     let searchSnipBtn = $('#search-snip-btn');
     let searchTagParam = $('#search-tag-param');
     let searchTextParam = $('#search-text-param');
 
+    // Add a tag to a specific snnippet in personal cache
     let addTagInput = $('.add-tag-input');
     let addTagBtn = $('.add-tag-btn');
 
+    // Remove a tag from snippet in personal cache
     let removeTagBtn = $('.remove-cache-tag');
     let cacheTag = $('.cache-snippet-badge ');
 
+    // delete entire snippet from personal cache
     let deleteSnip = $('.delete-snippet');
 
+    // selectors to populate each url and snippet tet in the users actual cache
     let cacheSnipURL = $('.cache-snip-url');
     let cacheSnipText = $('.cache-snip-text');
 
-    let publicViewBtn = $('.public-view');
+    // SELECTORS FOR PERSONAL PROJECTS BEGIN    
 
+    //Class to input the project title and description on the main screen
+    let homeProjTitle = $('.home-proj-title');
+    let homeProjDesc = $('.home-proj-desc');
+
+    //Selector for adding a a new project, optionally public
+    let newProjName = $('#new-proj-name');
+    let newProjDesc = $('#new-proj-desc');
+    let newProjBtn = $('#new-proj-btn');
+    let projPublicCheck = $('#public-proj');
+
+    // Selectors for changing the features of the project, and deleting it
+    let publicViewBtn = $('.public-view');
     let changeProjName = $('.change-proj-name');
     let changeProjDesc = $('.change-proj-desc');
     let changeProjBtn = $('.change-proj-btn');
 
-
+    // Selectors for adding a new snippet object to a certain project
     let newProjSnipURL = $('.new-proj-snip-url');
     let newProjSnipTag = $('.new-proj-snip-tag');
     let newProjSnipText = $('.new-proj-snip-text');
     let newProjSnipBtn = $('.new-proj-snip-btn');
 
+    // Selector for deleting a whole project
     let deleteProject = $('.delete-project');
 
+    // Selectors for changing the viewing rights of a specific user on a project. Filtered by the ID of the user
     let changeRightsId = $('.u-id');
     let adminBtn = $('.admin-btn');
     let viewBtn = $('.view-btn');
 
-    let deleteProjUrl = $('.delete-proj-url'); 
+    // delete URL snippet from specific project
+    let deleteProjUrl = $('.delete-proj-url');
+    //Selectors for populating the URL and created at from a project
+    let projURLCreated = $('.url-created-date');
+    let projURL = $('.proj-url');
 
-    let searchProjInput = $('.search-proj-input'); 
-    let searchProjBtn = $('.search-proj-btn'); 
-    let searchProjTextParam = $('.proj-search-text-param')
-    let searchProjTagParam = $('.proj-search-tag-param')
+    // Search features for a project, filtered by either tag or text
+    let searchProjInput = $('.search-proj-input');
+    let searchProjBtn = $('.search-proj-btn');
+    let searchProjTextParam = $('.proj-search-text-param');
+    let searchProjTagParam = $('.proj-search-tag-param');
 
-    let projURLCreated = $('.url-created-date'); 
-    let projURL = $('.proj-url'); 
-
-    let projSnipCreated = $('.proj-snip-created-date'); 
+    //Selectors for populating the text and created date from a project
+    let projSnipCreated = $('.proj-snip-created-date');
     let projText = $('.proj-text');
 
-    let homeProjTitle = $('.home-proj-title'); 
-    let homeProjDesc = $('.home-proj-desc'); 
+    // Snippet specific to a project, add and delete tags, and and delete the whole snippet 
+    let projTags = $('.projTags');
+    let addProjTag = $('.moveInput-add');
+    let delProjTagBtn = $('.del-proj-tag-btn');
+    let addProjTagBtn = $('.add-proj-tag-btn');
+    let delProjSnip = $('.delete-proj-snippet') 
 
-    let projTags = $('.projTags'); 
-    let delProjTag = $('.moveInput-del'); 
-    let addProjTag = $('.moveInput-del'); 
-    let delProjTagBtn = $('.del-proj-tag-btn'); 
-    let addProjTagBtn = $('.add-proj-tag-btn'); 
+    // Flags for removing tags from projects and personal cache
+     let removeTagBool = false ; 
+     let removeProjTagBool = false; 
 
 
+    // Allow the user to log out
     logOutBtn.click(function () {
         event.preventDefault();
         console.log('Log out')
     });
 
+    // query reddit for some information
     searchredditBtn.click(function () {
         event.preventDefault();
         console.log(redditQuery.val());
-        console.log('hey')
+        console.log('search reddit')
     });
 
+    // clear the reddit element
     redditClear.click(function () {
         event.preventDefault();
         console.log('clear Reddit');
     })
 
+    // Button click event for when the user adds a snippet to their personal cache
     addSnipBtn.click(function () {
         event.preventDefault();
         console.log(newSnipName.val());
@@ -137,46 +182,59 @@ $(document).ready(function () {
         console.log(newSnipTag.val());
     });
 
+    // Button click event for searching through cache, if conditions used to verify how they filter their search
     searchSnipBtn.click(function () {
         event.preventDefault();
         console.log(searchTagInput.val());
 
         if (searchTagParam.is(':checked')) {
             console.log('search tag')
+
         }
 
         if (searchTextParam.is(':checked')) {
             console.log('search text')
         }
+
+        // Need to build a query based on the text user gave and what they have checked
     });
 
+    // Add tag click event when adding to personal cache
     addTagBtn.click(function () {
         console.log(addTagInput.val());
     });
 
 
 
+    // remove a tag from a specific snippet in personal cache
     removeTagBtn.click(function () {
-        removeTagBool = true;
-        cacheTag.addClass("bg-danger");
+        if (!removeTagBool) {
+            removeTagBool = true;
+            cacheTag.addClass("bg-danger");
+        } else {
+            removeTagBool = false;
+            cacheTag.removeClass("bg-danger");
+        }
 
     });
 
+    // once a user has chosen remove tag, all tags become red and can then be selected for deletion
     cacheTag.click(function () {
         if (removeTagBool) {
+            let removedTag = $(this).text();
             cacheTag.removeClass("bg-danger");
-            console.log($(this).text())
+            console.log(removedTag);
             removeTagBool = false;
         }
     });
 
 
-
+    // delete a snippet from the users personal cache
     deleteSnip.click(function () {
         console.log('Delete Snippet')
     });
 
-
+    // Click event for starting a new project, filtered by a check if the user wants it to be public
     newProjBtn.click(function () {
         event.preventDefault();
         console.log(newProjDesc.val());
@@ -187,8 +245,20 @@ $(document).ready(function () {
         }
     });
 
+
+
+    //Admin rights for changing a project, filtered
+    changeProjBtn.click(function () {
+        event.preventDefault();
+
+        let changedName = changeProjName.val();
+        let changedDesc = changeProjDesc.val();
+        console.log(changedName);
+        console.log(changedDesc);
+    });
+
+    // Change the view of a project froom public to private or vice versa
     publicViewBtn.click(function () {
-        console.log($(this));
 
         if ($(this).is(':checked')) {
             console.log('public')
@@ -197,13 +267,8 @@ $(document).ready(function () {
         }
     });
 
-    changeProjBtn.click(function () {
-        event.preventDefault();
-        console.log(changeProjName.val());
-        console.log(changeProjDesc.val());
-    });
 
-
+    // Add a new snippet object to a certain project
     newProjSnipBtn.click(function () {
         event.preventDefault();
         console.log(newProjSnipURL.val());
@@ -211,17 +276,18 @@ $(document).ready(function () {
         console.log(newProjSnipText.val());
     });
 
+    // Delete a specific project
     deleteProject.click(function () {
         console.log('Delete Project')
     });
 
 
+    // Change the rights of each user, button finds the ID of that user to filter query
     [adminBtn, viewBtn].forEach(element => {
         element.click(function () {
             event.preventDefault();
             selection = $(this);
             userID = selection.closest('td').siblings('.u-id').text();
-
             if (selection.text() === 'View') {
                 console.log(`Viewer rights where user id is ${userID}`)
             }
@@ -229,21 +295,20 @@ $(document).ready(function () {
             if (selection.text() === 'Admin') {
                 console.log(`Admin rights where user id is ${userID}`)
             }
-
-
         });
 
 
     });
 
+
+    // click event for deleting a URL from a specific project
     deleteProjUrl.click(function () {
-        console.log('Delete Project');
-        console.log(this)
+        console.log('Delete URL');
     });
 
-
+    // Search through a project with optional parameters
     searchProjBtn.click(function () {
-        console.log(searchProjInput.val()); 
+        console.log(searchProjInput.val());
 
         if (searchProjTextParam.is(':checked')) {
             console.log('search tag')
@@ -252,30 +317,39 @@ $(document).ready(function () {
         if (searchProjTagParam.is(':checked')) {
             console.log('search text')
         }
-    }); 
-
-    delProjTagBtn.click(function () {
-        console.log(delProjTag.val()); 
-        removeProjTagBool = true;
-        projTags.addClass("bg-danger");
     });
 
+
+    // Delete a snippet from the console of the project you are on
+    delProjSnip.click(function () {
+        console.log('Delete this snippet')
+    })
+
+    // When the delete button is clicked, turn on flag for tag to be deleted and set color to red
+    delProjTagBtn.click(function () {
+        if(!removeProjTagBool) {
+            removeProjTagBool = true;
+            projTags.addClass("bg-danger");
+        } else {
+            removeProjTagBool = false;
+            projTags.removeClass("bg-danger");
+        }
+
+    });
+
+    // when you click a project tag, if the delete flag is on, register the tag for a query and turn off the flag
     projTags.click(function () {
-        if (removeTagBool) {
+        if (removeProjTagBool) {
             projTags.removeClass("bg-danger");
             console.log($(this).text())
-            removeprojTagBool = false;
+            removeProjTagBool = false;
         }
     });
 
+    // Add a tag to the snippet in a project console. Grab the value of the input
     addProjTagBtn.click(function () {
-        console.log(addProjTag.val())
+        console.log(addProjTag.val());
     });
-
-
-
-
-
 
 
 
--- public/js/projects.js
+++ public/js/projects.js
@@ -2,53 +2,53 @@
 
 $(document).ready(function () {
 
-  // instantiate the list.js library for searching capabilities
+  // instantiate the list.js library for searching through the tags
   let options = {
     valueNames: ['searchTags']
   }; 
   let userList = new List('search-tags', options);
 
-  // Selectors
+  //variable to store the user Information
+  let userID = 'ID retrieved from database'
+
+  // User Action buttons
   let userName = $('#user-name'); 
   let homeBtn = $('#home-btn'); 
   let logOutBtn = $('#logOut-btn'); 
-  let searchInput = $('.search'); 
-  let searchClearBtn = $('#clear-search'); 
+
+  //Search through the tags
   let searchTagBtn = $('.searchTags'); 
 
+  // specific project selectors. used a class name because each project must be unique
   let projName = $('.proj-name');
   let projDesc = $('.proj-desc');
   let projTags = $('.proj-tags');
+  let joinBtn = $('.join-btn');  
 
-  let clearSearch = $('#clear-search')
- 
-  let joinBtn = $('.join-btn'); 
-
-  let searchTags = $('.searchTags'); 
-
+  // User able to log out
   logOutBtn.click(function () {
     event.preventDefault(); 
     console.log('Log Out')
   }); 
 
-  searchClearBtn.click(function () {
-    searchInput.empty()
-  }); 
-
-  searchTags.click(function () {
-    console.log($(this).text()); 
+  // when a tag is clicked register the name and request all projects with that tag
+  searchTagBtn.click(function () {
+    let selectedTag = $(this).text(); 
+    console.log(selectedTag); 
   });
   
+  // when the join button is clicked
   joinBtn.click(function () {
     event.preventDefault(); 
-    console.log($(this).siblings('.proj-name').text()); 
+
+    //Get the name of the specfic project that the user has clicked on
+    let projectJoinName = $(this).siblings('.proj-name').text(); 
+
+    console.log(projectJoinName);  
+
 
   }); 
 
-  clearSearch.click(function () {
-    searchInput.trigger(':reset');  
-    console.log('hey')
-  }); 
 
 
 
