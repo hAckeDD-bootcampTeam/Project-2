27a537c136c7cd79580eeb2c4e8a6a30a5af68ba
Elizabeth P <ejpor@DESKTOP-A373663>
2018-08-12 12:57:10 -0400

    latest models-taggable

--- server.js
+++ server.js
@@ -26,75 +26,75 @@ app.set('view engine', 'handlebars');
 //E's testing-seeding table
 db.sequelize.sync({ force: true }).then(function () {
 	//db.sequelize.sync().then(function () {
-	// db.Users.bulkCreate([
-	// 	{ displayName: 'Elizabeth', email: 'elizabeth.porter.pevec@gmail.com', hashed_email_pw: 'AABBCC123', google: '', hashed_google_pw: '' },
-	// 	{ displayName: 'Angelina', email: '', hashed_email_pw: '', google: 'angelinasemail@gmail.com', hashed_google_pw: 'DDEEFF321' },
-	// 	{ displayName: 'David', email: '', hashed_email_pw: '', google: 'davidsemail@gmail.com', hashed_google_pw: 'GGHHII567' },
-	// 	{ displayName: 'Derek', email: 'dereksemail@gmail.com', hashed_email_pw: 'JJKKLL765', google: '', hashed_google_pw: '' }
-	// ]).then(users => {
-	// 	console.log(users)
-	// });
-	// db.Fullurls.bulkCreate([
-	// 	{ url: 'https://www.reddit.com/r/node/comments/8bf4gb/what_are_your_experiences_with_the_sequelize_orm/', pagetitle: 'What are your experiences with the sequelize ORM?', userdescription: '' },
-	// 	{ url: 'https://stackoverflow.com/questions/12487416/how-to-organize-a-node-app-that-uses-sequelize', pagetitle: 'How to organize a node app that uses sequelize?', userdescription: '' },
-	// 	{ url: 'https://www.danflyingsolo.com/why-learn-to-scuba-dive-padi/', pagetitle: 'Why learning to Scuba Dive will change your life', userdescription: '' }
-	// ]).then(furls => {
-	// 	// console.log(furls)
-	// });
-	// db.Projectgroupmembers.bulkCreate([
-	// 	{ membertype: 'Admin' },
-	// 	{ membertype: 'Editor' },
-	// 	{ membertype: 'Admin' },
-	// 	{ membertype: 'Admin' }
-	// ]).then(members => {
-	// 	//console.log(members)
-	// });
-	// db.Owners.bulkCreate([
-	// 	{ upvotes: '2', downvotes: '3', owner_type: 'Users' },
-	// 	{ upvotes: '2', downvotes: '3', owner_type: 'Projects' },
-	// 	{ upvotes: '2', downvotes: '3', owner_type: 'Users' },
-	// ]).then(reference => {
-	// 	//console.log(reference)
-	// });
-	// db.Projects.bulkCreate([
-	// 	{ projectName: 'Trilogy Group Project # 2', description: 'Our second project, which is marked as private', accesstype: 'Public' },
-	// 	{ projectName: 'Guitar Lessons', description: 'Place to keep our guitar-related info', accesstype: 'Private' },
-	// 	{ projectName: 'Trip to Rome!', description: 'All the fun things we can do in Rome', accesstype: 'Public' },
-	// ]).then(project => {
-	// 	//console.log(project)
-	// });
-	// db.Snippets.bulkCreate([
-	// 	{ snippet: 'This is some interesting text about data structures' },
-	// 	{ snippet: 'Here is some text relating to Sequelize' },
-	// 	{ snippet: 'This text was copied from a travel blog' },
-	// 	{ snippet: 'this sure is interesting' }
-	// ]).then(snip => {
-	// 	// console.log(snip)
-	// });
-	// db.Tags.bulkCreate([
-	// 	{ tag: 'Javascript' },
-	// 	{ tag: 'Travel' },
-	// 	{ tag: 'Programming' },
-	// 	{ tag: 'Cars' }
-	// ]).then(tag => {
-	// 	// console.log(tag)
-	// });
-	// db.Taggable.bulkCreate([
-	// 	{ tag_id: '1', taggable: 'Fullurls', taggable_id: '2' },
-	// 	{ tag_id: '1', taggable: 'Fullurls', taggable_id: '3' },
-	// 	{ tag_id: '2', taggable: 'Snippets', taggable_id: '1' },
-	// 	{ tag_id: '5', taggable: 'Fullurls', taggable_id: '3' }
-	// ]).then(taggable => {
-	// 	// console.log(taggable)
-	// });
-	// db.Commentable.bulkCreate([
-	// 	{ commenttext: 'Hi, my name is Derek and I approve of this message' },
-	// 	{ commenttext: 'I think that this link is really helpful - David' },
-	// 	{ commenttext: 'This sounds perfect for what we want to accomplish - Angelina' },
-	// 	{ commenttext: 'Elizabeth: There are some really good data structure examples here' }
-	// ]).then(comment => {
-	// 	//console.log(comment)
-	// });
+	db.Users.bulkCreate([
+		{ displayName: 'Elizabeth', email: 'elizabeth.porter.pevec@gmail.com', hashed_email_pw: 'AABBCC123', google: '', hashed_google_pw: '' },
+		{ displayName: 'Angelina', email: '', hashed_email_pw: '', google: 'angelinasemail@gmail.com', hashed_google_pw: 'DDEEFF321' },
+		{ displayName: 'David', email: '', hashed_email_pw: '', google: 'davidsemail@gmail.com', hashed_google_pw: 'GGHHII567' },
+		{ displayName: 'Derek', email: 'dereksemail@gmail.com', hashed_email_pw: 'JJKKLL765', google: '', hashed_google_pw: '' }
+	]).then(users => {
+		console.log(users)
+	});
+	db.Fullurls.bulkCreate([
+		{ url: 'https://www.reddit.com/r/node/comments/8bf4gb/what_are_your_experiences_with_the_sequelize_orm/', pagetitle: 'What are your experiences with the sequelize ORM?', userdescription: '' },
+		{ url: 'https://stackoverflow.com/questions/12487416/how-to-organize-a-node-app-that-uses-sequelize', pagetitle: 'How to organize a node app that uses sequelize?', userdescription: '' },
+		{ url: 'https://www.danflyingsolo.com/why-learn-to-scuba-dive-padi/', pagetitle: 'Why learning to Scuba Dive will change your life', userdescription: '' }
+	]).then(furls => {
+		// console.log(furls)
+	});
+	db.Projectgroupmembers.bulkCreate([
+		{ membertype: 'Admin' },
+		{ membertype: 'Editor' },
+		{ membertype: 'Admin' },
+		{ membertype: 'Admin' }
+	]).then(members => {
+		//console.log(members)
+	});
+	db.Owners.bulkCreate([
+		{ upvotes: '2', downvotes: '3', owner_type: 'Users' },
+		{ upvotes: '2', downvotes: '3', owner_type: 'Projects' },
+		{ upvotes: '2', downvotes: '3', owner_type: 'Users' },
+	]).then(reference => {
+		//console.log(reference)
+	});
+	db.Projects.bulkCreate([
+		{ projectName: 'Trilogy Group Project # 2', description: 'Our second project, which is marked as private', accesstype: 'Public' },
+		{ projectName: 'Guitar Lessons', description: 'Place to keep our guitar-related info', accesstype: 'Private' },
+		{ projectName: 'Trip to Rome!', description: 'All the fun things we can do in Rome', accesstype: 'Public' },
+	]).then(project => {
+		//console.log(project)
+	});
+	db.Snippets.bulkCreate([
+		{ snippet: 'This is some interesting text about data structures' },
+		{ snippet: 'Here is some text relating to Sequelize' },
+		{ snippet: 'This text was copied from a travel blog' },
+		{ snippet: 'this sure is interesting' }
+	]).then(snip => {
+		// console.log(snip)
+	});
+	db.Tags.bulkCreate([
+		{ tag: 'Javascript' },
+		{ tag: 'Travel' },
+		{ tag: 'Programming' },
+		{ tag: 'Cars' }
+	]).then(tag => {
+		// console.log(tag)
+	});
+	db.Taggable.bulkCreate([
+		{ tag_id: '1', taggable: 'Fullurls', taggable_id: '2' },
+		{ tag_id: '1', taggable: 'Fullurls', taggable_id: '3' },
+		{ tag_id: '2', taggable: 'Snippets', taggable_id: '1' },
+		{ tag_id: '5', taggable: 'Fullurls', taggable_id: '3' }
+	]).then(taggable => {
+		// console.log(taggable)
+	});
+	db.Commentable.bulkCreate([
+		{ commenttext: 'Hi, my name is Derek and I approve of this message' },
+		{ commenttext: 'I think that this link is really helpful - David' },
+		{ commenttext: 'This sounds perfect for what we want to accomplish - Angelina' },
+		{ commenttext: 'Elizabeth: There are some really good data structure examples here' }
+	]).then(comment => {
+		//console.log(comment)
+	});
 
 	app.listen(PORT, function () {
 		console.log("App listening on PORT" + PORT);
